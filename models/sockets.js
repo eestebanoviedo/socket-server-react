98d3379c8da393fd9f2528f22cf96892b76e575e
Esteban <ooviedoesteban@gmail.com>
2022-05-31 23:55:38 -0300

    make it better

--- models/sockets.js
+++ models/sockets.js
@@ -7,17 +7,10 @@ class Sockets {
   SocketsEvents() {
     //onconnection
     this.io.on("connection", (socket) => {
-      // socket.emit("mensaje-bienvenida", {
-      //   msg: "Bienvenido al server",
-      //   fecha: new Date(),
-      // })
-      //Escuchar el evento:mensaje-to-server
       socket.on("mensaje-to-server", (data) => {
+        console.log(data)
         this.io.emit("mensaje-from-server", data)
       })
-      // socket.on("mensaje-cliente", (data) => {
-      //   console.log("El front emitio algo: ", data)
-      // })
     })
   }
 }
